b42cf64893ef9380d5c4011c14a04f4bbe9e9501
Adypr <workady2015@gmail.com>
2019-10-16 14:18:01 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function zeros(expression) {
   // your solution
+
+  const arr = expression.split('*');
+
+  const res = arr.reduce((acc, elem) => {
+    const step = str => /!!/.test(str) ? 2n : 1n;
+    const factorial = num => num <= 1n ? 1n : BigInt(num) * factorial(BigInt(num) - step(elem));
+    return factorial(parseInt(elem)) * acc;
+  }, 1n);
+  return /0+$/.test(String(res)) ? String(res).match(/0+$/).join('').length : 0;
 }
